b1695d57a0b996aeb31df174da36d299d6fc584c
pumbaabetiverse <pumbaa@betiverse.org>
2025-05-01 22:32:13 +0700

    feat(telegram): add TelegramListener to module providers
    Added TelegramListener to the TelegramModule to enable handling of additional Telegram-specific events. This change improves modularity and paves the way for future enhancements or features.
    
    No breaking changes.

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TelegramService } from '@/telegram/telegram.service';
 import { TelegramUpdate } from '@/telegram/telegram.update';
 import { SettingModule } from '@/setting/setting.module';
+import { TelegramListener } from '@/telegram/telegram.listener';
 
 @Module({
   imports: [SettingModule],
-  providers: [TelegramService, TelegramUpdate],
+  providers: [TelegramService, TelegramUpdate, TelegramListener],
   exports: [TelegramService],
 })
 export class TelegramModule {}
